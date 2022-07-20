f3b54375c46032bb5a3e8420919709ce3a917c84
joshdaos <joshuadaos1998@gmail.com>
2022-07-19 23:23:22 -0700

    serving html pages

--- routes/shop.js
+++ routes/shop.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
@@ -5,7 +7,7 @@ const router = express.Router();
 // Route handling middleware
 router.get('/', (req, res, next) => {
     // console.log('In another middleware');
-    res.send('<h1>Hello World!</h1>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router
